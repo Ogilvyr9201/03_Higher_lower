ab48bd3d81f8a987c6fd04905fc5fb37d3ea786c
Ogilvyr9201 <78511761+Ogilvyr9201@users.noreply.github.com>
2021-05-04 14:35:46 +1200

    Post holiday upload

--- 00_hl_base.py
+++ 00_hl_base.py
@@ -5,13 +5,20 @@ import math
 
 # Functions go here
 # Ultimate number checker works for 4 situations!!!!!
+# checks for intgers.  Optinally checks an integer is above a minimum / below a maximum
 def num_check(question, error, low=None, high=None):
 
     valid = False
     while not valid:
         try:
-            response = int(input(question))
+            response = input(question).lower()
 
+            if response == "xxx":
+                return response
+            else:
+                response = int(response)
+
+            # Checks between two integers (includes endpoints)
             if low is not None and high is not None:
                 if low <= response <= high:
                     return response
@@ -20,6 +27,7 @@ def num_check(question, error, low=None, high=None):
                     print()
                     continue
 
+            # Checks for a number more than a minimum (no max)
             elif low is not None:
                 if response > low:
                     return response
@@ -88,7 +96,7 @@ def yes_no(question):
             print()
 
 
-# Instructions are here
+# Prints instructions if necessary, returns <blank> 
 def instructions():
     print("**** How to Play ****")
     print()
@@ -98,13 +106,11 @@ def instructions():
 
 
 # Main Routine
-rounds_played = 0
 choose_instruction = "Choose a number between 1 and 100"
 
 
 # Ask user if they have played before
 played_before = yes_no("Have you played this game before? ")
-print()
 
 if played_before == "no":
     instructions()
@@ -121,46 +127,159 @@ if mode_choice == 1:
 
 elif mode_choice == 2:
     mode_heading = "Mode #2: User choice"
-    low_boundary = num_check("What is the low boundary? ", "<error> please Eneter a number above 0", 0, None)
+    low_boundary = num_check("What is the low boundary? ", "<error> please enter a number above 0", 0, None)
 
     print()
     high_boundary = num_check("What is the high boundary? ", "<error> please enter a number above {}".format(low_boundary), low_boundary, None)
 
-print
+print()
 print(mode_heading)
+print()
 
-# Ask user how many rounds the want to play
-rounds = check_rounds()
+game_repeat = ""
+while game_repeat == "":
 
-end_game = ""
-while end_game == "":
+    # 0n reapeats sets all varibles from last game back too 0
+    rounds_played = 0
+    rounds_won = 0
+    rounds_lost = 0
 
-    # rounds heading
+    # Games result list (filled when you win or lose)
+    game_summary = []
+
+    # Ask user how many rounds the want to play and sets rounds played to 0 
+    rounds = check_rounds()
+    rounds_played = 0
+
+    end_game = "no"
+    while end_game == "no":
+
+        # rounds heading
+        if rounds == "":
+            round_heading = "Continuous Mode Round: Round {}".format(rounds_played + 1)
+
+        else:
+            round_heading = "Round {} of {}".format(rounds_played + 1, rounds)
+            # End game if Rounds played is smae as number of rounds
+            if rounds == rounds_played + 1:
+                end_game = "yes"
+
+        print()
+        print(round_heading)
+        print()
+
+        # Generate computer choice
+        comp_choice = random.randint(low_boundary, high_boundary)
+        print(comp_choice)
+        print()
+
+        # Calculate number of guesses
+        range = high_boundary - low_boundary + 1
+        max_raw = math.log2(range)
+        max_upped = math.ceil(max_raw)
+        max_guesses = max_upped + 1
+        print("Max Guesses: {}".format(max_guesses))
+        print()
+
+        # defines guess and number of guesses so loop can continue
+        guess = -1
+        num_guess = 0
+
+        # already guessed list (filled when you guess)
+        already_guessed = []
+
+        # looped untill user guesses corectly
+        while guess != comp_choice:
+
+            # Gets user guess
+            guess_instruction = "Guess a number between {} and {}: ".format(low_boundary, high_boundary)
+            guess_error = "<error> please choose an interger between {} and {}".format(low_boundary, high_boundary)
+
+            guess = num_check(guess_instruction, guess_error, low_boundary, high_boundary)
+            print()
+
+            # if user guess is the same as what they have guessed before
+            if guess in already_guessed:
+                print("You have already Guessed this number ")
+                print("You still have {} guesses left".format(guesses_left))
+                print()
+                continue
+
+            # adds guess to list of already guessed
+            already_guessed.append(guess)
+
+            # Compare guess with computer choice
+            if guess != comp_choice:
+                if guess < comp_choice:
+                    print("To Low try again ")
+                    print()
+                elif guess > comp_choice:
+                    print("To High try again ")
+                    print()
+                
+                # Adds users guesses
+                num_guess += 1
+            elif guess == comp_choice:
+                print("Congratulations! You got it right!")
+                print()
+                # Adds too rounds won
+                rounds_won += 1
+                # Adds users guesses
+                num_guess += 1
+                # Defines Result
+                result = "won" 
+                break
+
+            # checks how many guesses are left
+            guesses_left = max_guesses - num_guess
+            if num_guess != max_guesses:
+                print("You have {} Guesses left".format(guesses_left))
+                print()
+            else:
+                print("You lose lmao")
+                # Adds to rounds lost
+                rounds_lost += 1
+                print()
+                # Defines Result
+                result = "lost"
+                break
+        
+        # Adds Game result to a list for history
+        game_summary.append("Round #{}: {}".format(rounds_played + 1, result))
+
+        # Next round
+        rounds_played += 1
+
+    # Calculate Game Stats
+    percent_win = rounds_won / rounds_played * 100
+    percent_lose = rounds_lost / rounds_played * 100
+
+    # Displays game stats with % values to the nearest whole number
+    print("**** Game Statistics ****")
+    print("Win: {}: ({:.0f}%)\nLoss: {}: ({:.0f}%)".format(rounds_won, percent_win, rounds_lost, percent_lose))
     print()
-    if rounds == "":
-        round_heading = "Continuous Mode Round: Round {}".format(rounds_played + 1)
 
+    # Asks user if they want to see there history
+    show_history = yes_no("would you like to see game history? ")
+    print()
+
+    # displays history if user says yes
+    if show_history == "yes":
+        print("**** Game History ****")
+        for game in game_summary:
+            print(game)
+
+        print()
+        print("Thanks for playing")
+
+    # Doesnt display history if user says no
+    elif show_history == "no":
+        print("Thanks for Playing")
+
+    print()
+    # To play again
+    play_again = yes_no("Do you want to reapeat? ")
+    if play_again == "yes":
+        game_repeat = ""
     else:
-        round_heading = "Round {} of {}".format(rounds_played + 1, rounds)
-
-    print(round_heading)
-
-    # Generate computer choice
-    comp_choice = random.randint(low_boundary, high_boundary)
-    print(comp_choice)
-    print()
-
-    # Calculate number of guesses
-    range = high_boundary - low_boundary + 1
-    max_raw = math.log2(range)
-    max_upped = math.ceil(max_raw)
-    max_guesses = max_upped + 1
-    print("Max Guesses: {}".format(max_guesses))
-    print()
-
-    end_game = yes_no("Do you want to reapeat? ")
-    if end_game == "yes":
-        end_game = ""
-        continue
-    else:
-        break
+        game_repeat = " "
--- 07_hl_user_guess_v1.py
+++ 07_hl_user_guess_v1.py
@@ -13,7 +13,7 @@ def num_check(question, error, low=None, high=None):
             response = int(input(question))
 
             if low is not None and high is not None:
-                if low < response < high:
+                if low <= response <= high:
                     return response
                 else:
                     print(error)
@@ -45,41 +45,46 @@ def num_check(question, error, low=None, high=None):
 
 
 # main routine
-#lists 
+# lists
 already_guessed = []
 
 
 # Looped for testing
+guess = -1
+
 loop = ""
 while loop == "":
 
     # gets comp choice for guessing between 1 - 100 for testing
     comp_choice = random.randint(1, 100)
-    print(comp_choice)
 
-    # Loop until user gets correct 
+    # Loop until user gets correct
     while guess != comp_choice:
         # gets user guess (between 1-100 for testing would be low and high boundarys)
-        guess = num_check("Guess a number between {} and {}", "<error> please choose an interger", 1, 100)
+        guess = num_check("Guess a number between 1 and 100: ", "<error> please choose an interger", 1, 100)
+        print()
 
         # Compares guess
         # if user guess is the same as what they have guessed before
         if guess in already_guessed:
             print("You have already Guessed this number ")
             print("You still have __ guesses left")
+            print()
             continue
-        
+
         # adds guess to list of already guessed
         already_guessed.append(guess)
 
         if guess < comp_choice:
             print("To Low try again ")
+            print()
             continue
         elif guess > comp_choice:
             print("To High try again ")
+            print()
             continue
         elif guess == comp_choice:
             continue
-        
-    print("congratulations")
 
+    print("Congratulations")
+    print()
