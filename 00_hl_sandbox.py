0d34f95fb4a8472c6d59844c7c8c499a0ee8db86
Ogilvyr9201 <78511761+Ogilvyr9201@users.noreply.github.com>
2021-05-10 09:56:05 +1200

    Re did the instructions
    looks epiic

--- 00_hl_sandbox.py
+++ 00_hl_sandbox.py
@@ -1,2 +1,8 @@
-print("           *")
-print("    Pico-8    ")
+print(
+    "I wonder if this is gonna help with\n"
+    "writing long para graphs which will\n"
+    "wake instructions look way more neat"
+)
+print(
+
+)
